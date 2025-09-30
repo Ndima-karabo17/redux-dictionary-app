1d54ef10558178f7b974342af4d9bf14c3c4e52f
Mhangwani Ndima Karabo <203499810+Ndima-karabo17@users.noreply.github.com>
2025-09-30 02:00:20 +0200

    Update App.tsx
    Completed it based on instructions and here I just have to call my pages to display on the screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import { DefinitionDisplay } from './components/DefinationDisplay'
+import { WordSearch } from './components/WordSearch'
 
 function App() {
   
   return (
     <>
-      
+      <h1>Dictionary</h1>
+      <WordSearch/>
+      <hr />
+      <DefinitionDisplay/>
     </>
   )
 }
